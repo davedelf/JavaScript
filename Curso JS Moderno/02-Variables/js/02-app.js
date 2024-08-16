d2b2d76d79bdd4605d23fa5a6e0f38cc4b68300a
111858-DelfinoDavid <davideliasdelfino@gmail.com>
2024-08-16 12:30:09 -0300

    02 - Variables

---
+++ Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,22 @@
+//Crear variables - let
+let producto;
+console.log(producto);
+
+//Reasignar valor
+producto="Tablet";
+
+producto="Monitor";
+
+console.log(producto);
+
+producto=20
+console.log(producto);
+
+producto=true
+console.log(producto);
+
+producto=null
+console.log(producto);
+
+//Mismas reglas de var aplican a leet, lo que las diferencia es el scope. Let define mejor el scope
+//por ello se usa en lugar de var.
