76549bcaa64977fd287b281533365020234167df
111858-DelfinoDavid <davideliasdelfino@gmail.com>
2024-08-21 15:01:27 -0300

    Update 01-app.js

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,15 @@
+const numeros = [10, 20, 30];
+const strings = ["a", "b", "c", "d"];
+const varios = ["a", 10, 3.9, false, null];
+
+const meses = new Array("Enero", "Febrero", "Marzo");
+
+console.log(numeros);
+console.log(strings);
+console.log(varios);
+console.log(meses);
+
+console.log(numeros[1]);
+console.log(strings[2]);
+console.log(meses[0]);
+console.log(varios[3]);
