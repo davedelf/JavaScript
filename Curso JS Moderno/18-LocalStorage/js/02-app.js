49807ed7852399a51667966dc7908791ba0e38ef
111858-DelfinoDavid <70242863+davedelf@users.noreply.github.com>
2024-09-19 15:01:41 -0300

    Obtener datos de LocalStorage
    Recuperar datos de localStorage y convertirlos a object

--- Curso JS Moderno/18-LocalStorage/js/02-app.js
+++ Curso JS Moderno/18-LocalStorage/js/02-app.js
@@ -1,1 +1,12 @@
-//
+//Obtener datos de LocalStorage
+
+const nombre=localStorage.getItem("nombre")
+console.log(nombre);
+
+//Si no existe el valor que deseamos obtener nos arroja null
+
+const productoJSON=localStorage.getItem("producto")
+console.log(JSON.parse(productoJSON)); //lo convertimos a object
+
+const meses=localStorage.getItem("meses")
+console.log(JSON.parse(meses));
