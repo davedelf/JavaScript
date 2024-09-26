60839db39bfbd365efa06e6c222b775dbc668559
davedelf <davideliasdelfino@gmail.com>
2024-09-26 18:13:36 -0300

    Validación del formulario

--- Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -5,6 +5,8 @@ const emailInput = document.querySelector("#email");
 const fechaInput = document.querySelector("#fecha");
 const sintomasInput = document.querySelector("#sintomas");
 
+const formulario = document.querySelector("#formulario-cita");
+
 //Objeto de cita
 const citaObj = {
   paciente: "",
@@ -14,20 +16,28 @@ const citaObj = {
   sintomas: "",
 };
 
-console.log(citaObj);
-
-
 //Eventos
 
 //Sincronizamos código con la interfáz, inyectamos dinámicamente. Funciona si el 'name' del input coincide con la propiedad del objeto
-pacienteInput.addEventListener("change",datosCita)
-propietarioInput.addEventListener("change",datosCita)
-emailInput.addEventListener("change",datosCita)
-fechaInput.addEventListener("change",datosCita)
-sintomasInput.addEventListener("change",datosCita)
+pacienteInput.addEventListener("change", datosCita);
+propietarioInput.addEventListener("change", datosCita);
+emailInput.addEventListener("change", datosCita);
+fechaInput.addEventListener("change", datosCita);
+sintomasInput.addEventListener("change", datosCita);
 
+formulario.addEventListener("submit", submitCita);
 
-function datosCita(e){
-    citaObj[e.target.name]=e.target.value
-    console.log(citaObj);
+function datosCita(e) {
+  citaObj[e.target.name] = e.target.value;
+  console.log(citaObj);
+}
+
+//Validación del formulario
+function submitCita(e) {
+  e.preventDefault();
+
+  if (Object.values(citaObj).some((valor) => valor.trim() === "")) {
+    console.log("Todos los campos son obligatorios");
+    return;
+  }
 }
