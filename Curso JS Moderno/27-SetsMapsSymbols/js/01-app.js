e9320bea5cba1609529ae9027a07865df9989d98
davedelf <davideliasdelfino@gmail.com>
2024-10-01 15:33:53 -0300

    Sets

---
+++ Curso JS Moderno/27-SetsMapsSymbols/js/01-app.js
@@ -0,0 +1,49 @@
+//Evita valores duplicados
+
+//Tiende a ser más rápido que un objeto o arreglo
+
+const carrito=new Set();
+
+console.log(carrito);
+
+//Set Methods
+
+carrito.add("Camisa")
+carrito.add("Disco #1")
+carrito.add("Disco #2")
+carrito.add("Disco #3")
+carrito.add("Disco #3")  //No vuelve a agregar Disco #3 - valores duplicados
+
+
+console.log(carrito.size);
+
+console.log(carrito.has("Camisa"));
+console.log(carrito.has("Guitarra"));
+
+carrito.delete("Disco #3")
+console.log(carrito.delete("Disco #3"));
+
+carrito.forEach(producto=>{
+    console.log(producto);
+})
+
+
+
+
+
+
+
+carrito.clear()
+
+
+
+
+
+console.log(carrito);
+
+//Del siguiente arreglo eliminar los duplicados
+const numeros=[10,20,30,40,50,10,20]
+
+const numerosSin=new Set(numeros)
+
+console.log(numerosSin);
