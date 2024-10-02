9086872b0736c0e0fd19adcbf620654e8955c191
davedelf <davideliasdelfino@gmail.com>
2024-10-02 16:00:33 -0300

    Heredar una clase importada

--- Curso JS Moderno/28-Modulos/js/app.js
+++ Curso JS Moderno/28-Modulos/js/app.js
@@ -6,6 +6,9 @@ import {
   Cliente,
 } from "./cliente.js";
 
+//Importar empresa
+import { Empresa } from "./empresa.js";
+
 console.log(nombreCliente);
 console.log(ahorro);
 
@@ -16,3 +19,6 @@ const cliente = new Cliente(nombreCliente, ahorro);
 console.log(cliente);
 
 console.log(cliente.mostrarInformacion());
+
+const empresa = new Empresa("Código con Juan", 100, "Aprendizaje en línea");
+console.log(empresa.mostrarInformacion());
