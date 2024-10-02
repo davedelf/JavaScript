37c07e349ffdc10a1a5a2b1617fe5c2a1de4287d
davedelf <davideliasdelfino@gmail.com>
2024-10-02 15:40:05 -0300

    Exportar e Importar Funciones

--- Curso JS Moderno/28-Modulos/js/app.js
+++ Curso JS Moderno/28-Modulos/js/app.js
@@ -1,4 +1,12 @@
-import { nombreCliente, ahorro } from "./cliente.js";
+import {
+  nombreCliente,
+  ahorro,
+  mostrarInformacion,
+  tieneSaldo,
+} from "./cliente.js";
 
 console.log(nombreCliente);
 console.log(ahorro);
+
+console.log(mostrarInformacion(nombreCliente, ahorro));
+tieneSaldo(ahorro);
--- Curso JS Moderno/28-Modulos/js/cliente.js
+++ Curso JS Moderno/28-Modulos/js/cliente.js
@@ -6,6 +6,18 @@
 //   window.nombreCliente = "Juan"; //window : lo agregamos a la ventana global => la variable tiene scope global => podemos acceder desde cualquier script
 // })();
 
+export const nombreCliente = "Juan"; //Permite exportar la variable e importarla en otros archivos
+export const ahorro = 200;
 
-export const nombreCliente="Juan" //Permite exportar la variable e importarla en otros archivos
-export const ahorro=200
+//Exportar funciones
+export function mostrarInformacion(nombre, ahorro) {
+  return `Cliente: ${nombre} - Ahrro: ${ahorro}`;
+}
+
+export function tieneSaldo(ahorro) {
+  if (ahorro > 0) {
+    console.log("Si tiene saldo");
+  } else {
+    console.log("No tiene saldo");
+  }
+}
