be1d50e9c2507fae508ca9f74caf407d9e434291
davedelf <davideliasdelfino@gmail.com>
2024-10-10 15:47:32 -0300

    Validación Campos

---
+++ Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -0,0 +1,79 @@
+(function () {
+  let DB;
+
+  const formulario = document.querySelector("#formulario");
+
+  document.addEventListener("DOMContentLoaded", () => {
+    conectarDB();
+    formulario.addEventListener("submit", validarCliente);
+  });
+
+  function conectarDB() {
+    //Conectamos a la DB
+    const abrirConexion = window.indexedDB.open("crm", 1);
+
+    abrirConexion.onerror = function () {
+      console.log("Hubo un error");
+    };
+
+    abrirConexion.onsuccess = function () {
+      DB = abrirConexion.result; //Almacenamos la instancia de conexión a la bd
+      console.log("Se ha conectado a la DB");
+    };
+  }
+
+  function validarCliente(e) {
+    e.preventDefault();
+
+    //Leer inputs
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const telefono = document.querySelector("#telefono").value;
+    const empresa = document.querySelector("#empresa").value;
+
+    if (nombre === "" || email === "" || telefono === "" || empresa === "") {
+      imprimirAlerta("Todos los campos son obligatorios", "error");
+      return;
+    }
+  }
+
+  function imprimirAlerta(mensaje, tipo) {
+    const alerta = document.querySelector(".alerta");
+    if (!alerta) {
+      const divMensaje = document.createElement("DIV");
+      divMensaje.classList.add(
+        "px-4",
+        "py-3",
+        "rounded",
+        "max-w-lg",
+        "mx-auto",
+        "mt-6",
+        "text-center",
+        "border",
+        "alerta"
+      );
+
+      if (tipo === "error") {
+        divMensaje.classList.add(
+          "bg-red-100",
+          "border-red-400",
+          "text-red-700"
+        );
+      } else {
+        divMensaje.classList.add(
+          "bg-green-100",
+          "border-green-400",
+          "text-green-700"
+        );
+      }
+
+      divMensaje.textContent = mensaje;
+
+      formulario.appendChild(divMensaje);
+
+      setTimeout(() => {
+        divMensaje.remove();
+      }, 3000);
+    }
+  }
+})();
