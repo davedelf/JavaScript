e81484bab1f8c17b2e67626377ecc814f4ff579c
davedelf <davideliasdelfino@gmail.com>
2024-10-15 16:02:32 -0300

    Eliminar registro

--- Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/app.js
+++ Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,5 +1,6 @@
 (function () {
-  let DB;
+  // let DB;
+  const listadoClientes = document.querySelector("#listado-clientes");
 
   document.addEventListener("DOMContentLoaded", () => {
     crearDB();
@@ -7,6 +8,8 @@
     if (window.indexedDB.open("crm", 1)) {
       obtenerClientes();
     }
+
+    listadoClientes.addEventListener("click", eliminarRegistro);
   });
 
   //Crea la base de datos de IndexDb
@@ -60,7 +63,6 @@
         if (cursor) {
           const { nombre, empresa, email, telefono, id } = cursor.value;
 
-          const listadoClientes = document.querySelector("#listado-clientes");
           listadoClientes.innerHTML += ` 
         <tr>
           <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -75,7 +77,7 @@
           </td>
           <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
               <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
           </td>
       </tr>
   `;
@@ -86,5 +88,28 @@
         }
       };
     };
+  }
+
+  function eliminarRegistro(e) {
+    if (e.target.classList.contains("eliminar")) {
+      const idEliminar = Number(e.target.dataset.cliente);
+      const confirmar = confirm("Deseas eliminar este cliente?");
+      if (confirmar) {
+        const transaction = DB.transaction(["crm"], "readwrite");
+        const objectStore = transaction.objectStore("crm");
+
+        objectStore.delete(idEliminar);
+
+        transaction.oncomplete = () => {
+          console.log("Cliente eliminado");
+
+          e.target.parentElement.parentElement.remove();
+        };
+
+        transaction.onerror = () => {
+          console.log("Hubo un error");
+        };
+      }
+    }
   }
 })();
