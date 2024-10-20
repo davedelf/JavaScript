f278c5b5bbfc513bbf90126a7f471fb85a528888
davedelf <davideliasdelfino@gmail.com>
2024-10-20 18:19:28 -0300

    Consultar un JSON

---
+++ Curso JS Moderno/35-FetchAPI/js/02-app.js
@@ -0,0 +1,20 @@
+//Consultar un JSON
+
+const cargarJSONBtn = document.querySelector("#cargarJSON");
+cargarJSONBtn.addEventListener("click", obtenerDatos);
+
+function obtenerDatos() {
+  const url = "data/empleado.json";
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((datos) => mostrarHTML(datos));
+}
+
+function mostrarHTML({ empresa, id, nombre, trabajo }) {
+  const contenido = document.querySelector(".contenido");
+  contenido.innerHTML = `
+    <p>Empleado: ${nombre}</p>
+    <p>ID: ${id}</p>
+    <p>Empresa: ${empresa}</p>
+    <p>Trabajo: ${trabajo}</p>`;
+}
