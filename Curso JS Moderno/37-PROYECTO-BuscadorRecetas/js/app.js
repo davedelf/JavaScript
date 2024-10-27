6ecfc591c160522362752ada77af1d9dffab06be
davedelf <davideliasdelfino@gmail.com>
2024-10-27 16:02:33 -0300

    Mostrar información en el Modal

--- Curso JS Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso JS Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,6 +1,7 @@
 const selectCategorias = document.querySelector("#categorias");
 selectCategorias.addEventListener("change", seleccionarCategoria);
 const resultado = document.querySelector("#resultado");
+const modal = new bootstrap.Modal("#modal", {});
 
 function iniciarApp() {
   obtenerCategorias();
@@ -72,6 +73,18 @@ function mostrarRecetas(recetas = []) {
     recetaButton.classList.add("btn", "btn-danger", "w-100");
     recetaButton.textContent = "Ver Receta";
 
+    //Modal de Bootstrap
+    // recetaButton.dataset.bsTarget="#modal"
+    // recetaButton.dataset.bsToggle="modal"
+
+    /**
+     * Usamos onclick porque este button no va a existir al momento de cargar
+     * el DOM sino que se generará dinámicamente según lo que seleccionemos
+     */
+    recetaButton.onclick = function () {
+      seleccionarReceta(idMeal);
+    };
+
     //Inyectamos en el HTML
     recetaCardBody.appendChild(recetaHeading);
     recetaCardBody.appendChild(recetaButton);
@@ -83,6 +96,36 @@ function mostrarRecetas(recetas = []) {
 
     resultado.appendChild(recetaContenedor);
   });
+
+  function seleccionarReceta(id) {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+      .then((respuesta) => {
+        return respuesta.json();
+      })
+      .then((resultado) => {
+        mostrarRecetaModal(resultado.meals[0]);
+      });
+  }
+
+  //Muestra el modal
+  function mostrarRecetaModal(receta) {
+    const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+    //Añadir contenido al modal
+    const modalTitle = document.querySelector(".modal .modal-title");
+    const modalBody = document.querySelector(".modal .modal-body");
+
+    modalTitle.textContent = strMeal;
+    modalBody.innerHTML = `
+    <img class="img-fluid" src="${strMealThumb}" alt="Receta ${strMeal}"/>
+    <h3 class="my-3">Instrucciones</h3>
+    <p>${strInstructions}</p>
+
+    `;
+
+    modal.show();
+  }
 
   function limpiarHTML(selector) {
     while (selector.firstChild) {
