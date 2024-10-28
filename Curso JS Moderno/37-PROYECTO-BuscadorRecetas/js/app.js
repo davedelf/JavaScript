c709ca2157c77b04c8fe8dafb94d356fe00eeb59
davedelf <davideliasdelfino@gmail.com>
2024-10-28 14:15:57 -0300

    Mostrar mensaje Toast al Agregar/Eliminar registro

--- Curso JS Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso JS Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
@@ -158,6 +158,7 @@ function mostrarRecetas(recetas = []) {
       if (existeStorage(idMeal)) {
         eliminarFavorito(idMeal);
         btnFavorito.textContent = "Guardar Favorito";
+        mostrarToast("Eliminado Correctamente");
         return;
       }
 
@@ -167,6 +168,7 @@ function mostrarRecetas(recetas = []) {
         img: strMealThumb,
       });
       btnFavorito.textContent = "Eliminar Favorito";
+      mostrarToast("Agregado Correctamente");
     };
 
     const btnCerrarModal = document.createElement("BUTTON");
@@ -199,6 +201,14 @@ function mostrarRecetas(recetas = []) {
   function existeStorage(id) {
     const favoritos = JSON.parse(localStorage.getItem("favoritos")) ?? [];
     return favoritos.some((favorito) => favorito.id === id);
+  }
+
+  function mostrarToast(mensaje) {
+    const toastDiv = document.querySelector("#toast");
+    const toastBody = document.querySelector(".toast-body");
+    const toast = new bootstrap.Toast(toastDiv);
+    toastBody.textContent = mensaje;
+    toast.show();
   }
 
   function limpiarHTML(selector) {
