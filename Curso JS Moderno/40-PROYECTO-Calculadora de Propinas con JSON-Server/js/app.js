55032ce64fd1237f7651eecb950713f1faf1a07d
davedelf <davideliasdelfino@gmail.com>
2024-10-31 17:42:52 -0300

    Validacion de campos

--- Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -4,10 +4,19 @@ let cliente = {
   pedido: [],
 };
 
-const btnGuardarCliente=document.querySelector("#guardar-cliente")
+const btnGuardarCliente = document.querySelector("#guardar-cliente");
 
-btnGuardarCliente.addEventListener("click",guardarCliente)
+btnGuardarCliente.addEventListener("click", guardarCliente);
 
-function guardarCliente(){
-    console.log("desde la funcion");
+function guardarCliente() {
+  const mesa = document.querySelector("#mesa").value;
+  const hora = document.querySelector("#hora").value;
+
+  //Otra forma de validar campos con el array method some
+  const camposVacios = [mesa, hora].some((campo) => campo === "");
+  if (camposVacios) {
+    console.log("Si hay al menos un campo vacío");
+  } else {
+    console.log("Todos los campos están llenos");
+  }
 }
