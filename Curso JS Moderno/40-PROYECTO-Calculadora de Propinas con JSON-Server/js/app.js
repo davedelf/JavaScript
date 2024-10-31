51fb488bef87f7a47e9e9588ce5498a94d7a2d17
davedelf <davideliasdelfino@gmail.com>
2024-10-31 17:25:31 -0300

    index.html

---
+++ Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,13 @@
+let cliente = {
+  mesa: "",
+  hora: "",
+  pedido: [],
+};
+
+const btnGuardarCliente=document.querySelector("#guardar-cliente")
+
+btnGuardarCliente.addEventListener("click",guardarCliente)
+
+function guardarCliente(){
+    console.log("desde la funcion");
+}
