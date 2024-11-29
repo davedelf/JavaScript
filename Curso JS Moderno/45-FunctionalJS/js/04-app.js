fcdf5345956d85afd0b7b1448fcb409efd8cbb51
davedelf <davideliasdelfino@gmail.com>
2024-11-29 13:17:02 -0300

    .Map en Functional JS

--- Curso JS Moderno/45-FunctionalJS/js/04-app.js
+++ Curso JS Moderno/45-FunctionalJS/js/04-app.js
@@ -1,10 +1,21 @@
 const carrito = [
-    { nombre: 'Monitor 20 Pulgadas', precio: 500},
-    { nombre: 'Televisión 50 Pulgadas', precio: 700},
-    { nombre: 'Tablet', precio: 300},
-    { nombre: 'Audifonos', precio: 200},
-    { nombre: 'Teclado', precio: 50},
-    { nombre: 'Celular', precio: 500},
-    { nombre: 'Bocinas', precio: 300},
-    { nombre: 'Laptop', precio: 800},
+  { nombre: "Monitor 20 Pulgadas", precio: 500 },
+  { nombre: "Televisión 50 Pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 },
 ];
+
+//Supongamos que queremos crear un nuevo arreglo en base al anterior pero que filtre por una determinada condición. Usamos .map
+//Recordamos que .forEach solo itera y .Map crea un nuevo array
+
+const obtenerNombres = (producto) => {
+  return producto.nombre;
+};
+
+const resultado = carrito.map(obtenerNombres);
+
+console.log(resultado);
