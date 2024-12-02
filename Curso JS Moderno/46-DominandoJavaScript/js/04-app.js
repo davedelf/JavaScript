e29459a139eab04cb144087b830fa3bd3fc2d5c1
davedelf <davideliasdelfino@gmail.com>
2024-12-02 12:06:02 -0300

    Implicit Binding

---
+++ Curso JS Moderno/46-DominandoJavaScript/js/04-app.js
@@ -0,0 +1,18 @@
+//Implicit Binding - Uso de this
+
+const usuario = {
+  nombre: "Juan",
+  edad: 20,
+  informacion() {
+    console.log(`Mi nombre es ${this.nombre} y mi edad es ${this.edad}`);
+  },
+  mascota: {
+    nombre: "Hook",
+    edad: 1,
+    informacion() {
+      console.log(`Mi nombre es ${this.nombre} y mi edad es ${this.edad}`);
+    },
+  },
+};
+usuario.informacion();
+usuario.mascota.informacion();
