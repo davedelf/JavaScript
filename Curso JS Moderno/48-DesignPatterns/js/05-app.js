c2b29e6c8875d51d9e880d47cc7d5560936ca485
davedelf <davideliasdelfino@gmail.com>
2024-12-04 15:53:10 -0300

    Module Pattern

---
+++ Curso JS Moderno/48-DesignPatterns/js/05-app.js
@@ -0,0 +1,7 @@
+//Module Pattern - Permite exportar funciones, variables y acceder a ellos desde otros archivos.
+
+const mostrarCliente = (nombre) => {
+  console.log(nombre);
+};
+
+export default mostrarCliente;
